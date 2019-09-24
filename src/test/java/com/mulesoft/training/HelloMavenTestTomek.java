a4cb8fe1e911714c790aa95fab600c3f107fcf21
tszymczuk <tomasz.szymczuk@pwc.com>
2019-09-24 11:59:05 +0200

    test4

---
+++ src/test/java/com/mulesoft/training/HelloMavenTestTomek.java
@@ -0,0 +1,20 @@
+package com.mulesoft.training;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+public class HelloMavenTestTomek extends FunctionalTestCase {
+
+    @Test
+    public void mavenFlowReturnsHelloMaven() throws Exception {
+        runFlowAndExpectProperty("mavenFlow", "status", "200");
+    }
+    
+    @Override
+    protected String getConfigFile() {
+        return "maven-project.xml";
+    }
+
+}
