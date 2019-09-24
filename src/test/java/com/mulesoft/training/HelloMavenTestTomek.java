2bf7d6528e86c65cec0e2447a619aab57c71997e
tszymczuk <tomasz.szymczuk@pwc.com>
2019-09-24 13:04:02 +0200

    test5

--- src/test/java/com/mulesoft/training/HelloMavenTestTomek.java
+++ src/test/java/com/mulesoft/training/HelloMavenTestTomek.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTestTomek extends FunctionalTestCase {
-
+//tttttt
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpectProperty("mavenFlow", "status", "200");
